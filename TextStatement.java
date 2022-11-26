1ab0b1b0acf0769dd1db97071158d74f389c8532
mtgo007 <mtgo007@gmail.com>
2022-11-26 19:03:40 -0300

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,34 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+  private String toStrHeader(String name) {
+    return ("Rental Record for " + name + "\n");
+  }
+
+  private String toStrRental(Rental aRental) {
+    return ("\t" + aRental.getMovie().getTitle() + "\t" + String.valueOf(aRental.getCharge()) + "\n");
+  }
+
+  private String toStrTotalCharge(double totalCharge) {
+    return ("Amount owed is " + String.valueOf(totalCharge) + "\n");
+  }
+
+  private String toStrTotalFrequentRenterPoints(int totalFrequentRenterPoints) {
+    return ("You earned " + String.valueOf(totalFrequentRenterPoints) + " frequent renter points");
+  }
+
   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
-      }
-      //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
-      return result;
-   }
+    Enumeration rentals = aCustomer.getRentals();
+    String result = toStrHeader(aCustomer.getName());
+
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      // show figures for this rental
+      result += toStrRental(each);
+    }
+    // add footer lines
+    result += toStrTotalCharge(aCustomer.getTotalCharge());
+    result += toStrTotalFrequentRenterPoints(aCustomer.getTotalFrequentRenterPoints());
+    return result;
+  }
 }
