f46d2b798d4a98584cd660ecbec72acb9ada1608
mtgo007 <mtgo007@gmail.com>
2022-11-26 15:42:17 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -16,6 +16,28 @@ public class Customer {
    public String getName (){
       return _name;
    }
+
+  private double amountFor(Rental each) {
+    double thisAmount = 0;
+    
+    switch (each.getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+             thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+       case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+       case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+             thisAmount += (each.getDaysRented() - 3) * 1.5;
+           break;
+    }
+
+    return thisAmount;
+  }
   
   public String statement() {
      double totalAmount = 0;
@@ -26,22 +48,8 @@ public class Customer {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        //determine amounts for each line  
+       thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
