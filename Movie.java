ed8e7ddba96c6633a9113154ea3862c5368aae80
mtgo007 <mtgo007@gmail.com>
2022-11-26 17:28:58 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -1,30 +1,42 @@
 public class Movie {
 
-  public static final int  CHILDRENS = 2;
-  public static final int  REGULAR = 0;
-  public static final int  NEW_RELEASE = 1;
+  public static final int CHILDRENS = 2;
+  public static final int REGULAR = 0;
+  public static final int NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
   public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+    _title = title;
+    setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
+    return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+    switch (arg) {
+      case REGULAR:
+        _price = new RegularPrice();
+        break;
+      case CHILDRENS:
+        _price = new ChildrensPrice();
+        break;
+      case NEW_RELEASE:
+        _price = new NewReleasePrice();
+        break;
+      default:
+        throw new IllegalArgumentException("Incorrect Price Code");
+    }
   }
 
-  public String getTitle (){
-      return _title;
+  public String getTitle() {
+    return _title;
   }
 
-  public double getCharge(int daysRented){
+  public double getCharge(int daysRented) {
     double thisAmount = 0;
 
     switch (getPriceCode()) {
@@ -46,15 +58,15 @@ public class Movie {
     return thisAmount;
   }
 
-  public int getFrequentRenterPoints(int daysRented){
+  public int getFrequentRenterPoints(int daysRented) {
     int frequentRenterPoints = 0;
-    
+
     // add frequent renter points
     frequentRenterPoints++;
     // add bonus for a two day new release rental
     if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
       frequentRenterPoints++;
-    
+
     return frequentRenterPoints;
   }
 }
